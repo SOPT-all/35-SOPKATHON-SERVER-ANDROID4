bb95be8828849ca6fe46ffe62bfccc7cdb20cbb6
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2024-11-24 05:33:30 +0900

    [feat] 3차 배포

--- src/main/java/com/example/sopkathon/domian/ping/service/PingService.java
+++ src/main/java/com/example/sopkathon/domian/ping/service/PingService.java
@@ -5,7 +5,6 @@ import com.example.sopkathon.domian.ping.dto.res.PingListRes;
 import com.example.sopkathon.domian.ping.repository.Ping;
 import com.example.sopkathon.domian.ping.enums.PingStatusType;
 import com.example.sopkathon.domian.ping.repository.PingRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
